5d74315307a225b4d93ceba02b348973a05874d0
Tayrlock <dkfear@ya.ru>
2022-02-04 20:42:26 +0300

    циклы

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 "use strict"
-const numberOfFilms = +prompt('Сколько фильмов вы посмотрели')
+let numberOfFilms = undefined
+
+while (numberOfFilms == '' || numberOfFilms < 0 || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('Сколько фильмов вы посмотрели')
+    if (numberOfFilms == '' || numberOfFilms < 0 || isNaN(numberOfFilms)) { alert('Произошла ошибка') }
+}
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -8,12 +14,23 @@ const personalMovieDB = {
     privat: false,
 }
 
-let a = prompt('Какой из последних фильмов вы посмотрели?');
-let b = prompt('На сколько вы его оцените?');
-
+let a = ''
+while (a == false || a.length > 50) {
+    a = prompt('Какой из последних фильмов вы посмотрели?');
+}
+let b = ''
+while (b == false || b.length > 50) {
+    b = prompt('На сколько вы его оцените?');
+}
 
 
 personalMovieDB.movies[a] = b
-
-
 console.log(personalMovieDB)
+
+/* if (personalMovieDB.count < 10 && personalMovieDB > 0) { alert('Просмотрено мало фильмов') }
+if (personalMovieDB.count > 10 && personalMovieDB.count < 30) { alert('Вы классный зритель') }
+if (personalMovieDB.count > 30) { alert('Вы киноман') } */
+
+if (personalMovieDB.count > 30) { alert('Вы киноман') }
+else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) { alert('Вы классный зритель') }
+else {alert('Просмотрено мало фильмов')}
